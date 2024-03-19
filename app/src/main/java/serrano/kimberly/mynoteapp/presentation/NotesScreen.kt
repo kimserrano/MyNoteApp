3292809cd098a8dedf73f483a2c8c4c24ef5f62b
kimserrano <alejandra.ser.ur@gmail.com>
2024-03-19 01:13:05 -0600

    muestra el mensaje para confirmar si se desea borrar

--- app/src/main/java/serrano/kimberly/mynoteapp/presentation/NotesScreen.kt
+++ app/src/main/java/serrano/kimberly/mynoteapp/presentation/NotesScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.material.icons.rounded.Sort
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -23,7 +24,10 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,6 +37,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import serrano.kimberly.mynoteapp.R
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -110,6 +118,40 @@ fun NoteItem(
     index: Int,
     onEvent: (NotesEvent) -> Unit
 ) {
+
+    var showDialog by remember { mutableStateOf(false) }
+
+    fun handleDeleteConfirmation(confirmDelete: Boolean) {
+        if (confirmDelete) {
+            onEvent(NotesEvent.DeleteNote(state.notes[index]))
+        }
+        showDialog = false
+    }
+    // mensaje
+    if (showDialog) {
+        AlertDialog(
+            onDismissRequest = {
+                showDialog = false
+            },
+            title = {
+                Text(text = "Do you want to delete this note?")
+            },
+            confirmButton = {
+                TextButton(onClick = {
+                    handleDeleteConfirmation(confirmDelete = true)
+                }) {
+                    Text("Yes")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = {
+                    handleDeleteConfirmation(confirmDelete = false)
+                }) {
+                    Text("No")
+                }
+            }
+        )
+    }
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -140,7 +182,7 @@ fun NoteItem(
 
         IconButton(
             onClick = {
-                onEvent(NotesEvent.DeleteNote(state.notes[index]))
+                showDialog = true
             }
         ) {
 
